406089014686f33012fdd75d319d77ae8791f55e
tanvirmohammadrizvy <tanvirmohammad2@gmail.com>
2022-06-21 18:08:42 +0600

    updated

--- FrontEnd/client/src/pages/home/Home.js
+++ FrontEnd/client/src/pages/home/Home.js
@@ -1,10 +1,12 @@
 import './home.css';
 import Navbar from '../../components/navbar/Navbar';
+import Header from '../../components/header/Header';
 
 function Home() {
   return (
     <div>
         <Navbar />
+        <Header />
     </div>
   )
 }
